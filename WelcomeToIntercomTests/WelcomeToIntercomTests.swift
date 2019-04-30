ce0cf4aa19bb2348cdd5a5e9f49f886e16dcd7ed
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-30 13:36:35 +0300

    test_cases

--- WelcomeToIntercomTests/WelcomeToIntercomTests.swift
+++ WelcomeToIntercomTests/WelcomeToIntercomTests.swift
@@ -19,14 +19,24 @@ class WelcomeToIntercomTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSingleCustomerDecoding() {
+        // This test will work if customers.txt file has only one customer json object
+        // I tested it with one line in customers.txt
         let bundle = Bundle(for: type(of: self))
         guard let url = bundle.url(forResource: "customers", withExtension: "txt"),
-            let _ = try? Data(contentsOf: url) else {
+            let data = try? Data(contentsOf: url) else {
                 return
         }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard let customer = try? decoder.decode(Customer.self, from: data) else {
+            return
+        }
+        
+        XCTAssertEqual(customer.userId, 1993)
+        XCTAssertEqual(customer.name, "Rozeri Dilar")
+        XCTAssertEqual(customer.latitude, "53")
+        XCTAssertEqual(customer.longitude, "-6")
     }
 
     func testPerformanceExample() {
